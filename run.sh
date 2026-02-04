7bd76432f917116428b6b5e2d8894bc15b6b74bf
prasadhonrao <honrao.prasad@gmail.com>
2026-02-04 17:03:05 +0000

    feat: update Dapr configuration ports and add run script for Inventory Service

--- run.sh
+++ run.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 # Inventory Service - Run with Dapr
-# Change to project root directory
-cd "$(dirname "$0")/.." || exit 1
 
 echo "Starting Inventory Service with Dapr..."
-echo "Working directory: $(pwd)"
 echo "Service will be available at: http://localhost:8005"
-echo "Dapr HTTP endpoint: http://localhost:3500"
-echo "Dapr gRPC endpoint: localhost:50001"
+echo "Dapr HTTP endpoint: http://localhost:3505"
+echo "Dapr gRPC endpoint: localhost:50005"
 echo ""
 
 dapr run \
   --app-id inventory-service \
   --app-port 8005 \
-  --dapr-http-port 3500 \
-  --dapr-grpc-port 50001 \
+  --dapr-http-port 3505 \
+  --dapr-grpc-port 50005 \
   --log-level info \
   --config ./.dapr/config.yaml \
   --resources-path ./.dapr/components \
